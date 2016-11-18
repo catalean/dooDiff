c7887e3b56eb753efc72f66270134ba07b9091b0
Catalin Kormos <catalin.kormos@gmail.com>
2016-11-18 11:22:32 +0100

    initial setup

--- src/main/java/ro/codebeat/doo/diff/model/Diff.java
+++ src/main/java/ro/codebeat/doo/diff/model/Diff.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ro.codebeat.dodiff.model;
+package ro.codebeat.doo.diff.model;
 
 /**
  * @author Catalin Kormos
